07412027a475adbf38e256feb040b3695c84a33c
hiperlogic <rodrigod@hiperlogic.com.br>
2020-06-14 17:02:24 -0300

    Use specific views methods as URL routing and assign names to routes

--- store_rest/store_api/urls.py
+++ store_rest/store_api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from rest_framework.routers import SimpleRouter
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from .views import (
     UserRegisterAPIView,
@@ -10,83 +11,28 @@ from .views import (
     OrderItemViewSet
 )
 
+router = SimpleRouter()
 
-urlpatterns = [
-    path('register/', UserRegisterAPIView.as_view({
-        'post': 'create'
-        },
-        name='register'
-    )),
-    path('users/', UserViewSet.as_view(
-        {
-            'get': 'list'
-        },
-        name='users'
-    )),
-    path('user/<int:pk>', UserViewSet.as_view(
-        {
-            'get': 'retrieve',
-            'put': 'update'
-        },
-        name='user'
-    )),
-    path('user/<int:pk>/orders', UserViewSet.as_view(
-        {
-            'get': 'orders'
-        },
-        name='user_orders'
-    )),
+app_name = 'store_api'
 
-    path('orders/', OrdersViewSet.as_view(
-        {
-            'get': 'list'
-        },
-        name='orders'
-    )),
-    path('orders/create', OrdersViewSet.as_view(
-        {
-            'post': 'create'
-        },
-        name='orders_create'
-    )),
-    path('order/<int:pk>', OrdersViewSet.as_view(
-        {
-            'get': 'retrieve',
-            'put': 'update'
-        },
-        name='order'
-    )),
+urlpatterns = format_suffix_patterns([
+    path('register/', UserRegisterAPIView.register_view(), name='register'),
+    path('users/', UserViewSet.users_view(), name='users'),
+    path('user/<int:pk>', UserViewSet.user_view(), name='user'),
+    path('user/<int:pk>/orders', UserViewSet.user_orders_view(), name='user_orders'),
 
-    path('order_items/',OrderItemViewSet.as_view({
-        'get': 'list',
-        'post': 'create'
-        },
-        name='order_items')),
-    path('order_item/<int:pk>', OrderItemViewSet.as_view({
-        'get': 'retrieve',
-        'put': 'update'
-        },
-        name='order_item')),
+    path('orders/', OrdersViewSet.orders_view(), name='orders'),
+    path('orders/create', OrdersViewSet.orders_create_view(), name='orders_create'),
+    path('order/<int:pk>', OrdersViewSet.order_view(), name='order'),
 
-    path('products/', ProductsViewSet.as_view({
-        'get': 'list'
-        },
-        name='products'
-    )),
-    path('products/create', ProductsViewSet.as_view({
-        'post': 'create'
-        },
-        name='products_create')),
-    path('product/<int:pk>', ProductsViewSet.as_view({
-        'get': 'retrieve',
-        'put': 'update'
-        },
-        name='product')),
-    path('product/high_orders', ProductsViewSet.as_view({
-        'get': 'high_orders',
-        },
-        name='product_high_order'))
-]
+    path('order_items/', OrderItemViewSet.order_items_view(), name='order_items'),
+    path('order_item/<int:pk>', OrderItemViewSet.order_item_view(), name='order_item'),
 
+    path('products/', ProductsViewSet.products_view(), name='products'),
+    path('products/create', ProductsViewSet.product_create_view(), name='products_create'),
+    path('product/<int:pk>', ProductsViewSet.product_view(), name='product'),
+    path('products/high_orders', ProductsViewSet.product_high_orders_view(), name='high_orders')
+])
 
+urlpatterns += router.urls
 
