fae6ef27f275ed62f3abadbb0e32d87d6194df1a
hiperlogic <rodrigod@hiperlogic.com.br>
2020-06-14 17:01:13 -0300

    Include namespace for the API

--- store_rest/store_rest/urls.py
+++ store_rest/store_rest/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('rest_framework.urls')),
-    path('', include('store_api.urls')),
+    path('', include('store_api.urls', namespace='store_api')),
 ]
+
+
